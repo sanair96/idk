fde80fc6dd90ea3c2e1cbebced00f1589110ef45
sai3010 <saiprasanth2007@gmail.com>
2016-08-02 22:08:36 +0530

    CIVIL PAGE

--- idk/qpnotesCIVIL.xaml.cs
+++ idk/qpnotesCIVIL.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,159 +28,16 @@ namespace idk
             this.InitializeComponent();
         }
 
-        private void ME_Tapped(object sender, TappedRoutedEventArgs e)
-        {
+      
 
-        }
-        private void ME1_Tapped(object sender, TappedRoutedEventArgs e)
+       async private void textBlock_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-        private void ME2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void ME3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void ME4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void ME5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
+            await Launcher.LaunchUriAsync(new Uri("https://1drv.ms/f/s!AiaDLQmq8IPrlB2l5xLmL2WzPKso"));
         }
 
-        private void US_Tapped(object sender, TappedRoutedEventArgs e)
+     async private void textBlock1_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-        private void US1_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void US2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void US3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void US4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void US5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void CD_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CD1_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CD2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CD3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CD4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CD5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void CN_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CN1_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CN2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CN3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CN4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void CN5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void OR_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void OR1_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void OR2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void OR3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void OR4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-        private void OR5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC1_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC2_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC3_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC4_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void DC5_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
+            await Launcher.LaunchUriAsync(new Uri("https://1drv.ms/f/s!AiaDLQmq8IPrlB2l5xLmL2WzPKso"));
         }
     }
 }
